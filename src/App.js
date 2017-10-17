da591fa7ca2423802b5395054fd7049380a9ae86
Cathy Lee <cathylee@cathys-mbp.lan>
2017-10-16 22:53:47 -0700

    Integrated bootstrap, created Footer and Header component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Header(props) {
+  return (
+    <header className="App-header container">
+      <div className="row">
+        <ul className="nav">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Works</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Resume</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <p>&copy; {new Date().toLocaleTimeString} Designed & Developed by Cathy Lee. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Cathy Lee</h1>
-          <p className="tagline">Design-saavy Front-end Developer</p>
-        </header>
+        <Header />
+        <div className="slider container">
+          <div className="row">
+            <h1 className="App-title">Cathy Lee</h1>
+            <p className="tagline">Design-saavy Front-end Developer</p>
+          </div>
+        </div>
         <p className="App-intro">
+        <img src={logo} className="App-logo" alt="logo" />
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+class Footer extends React.Component {
+	render() {
+		return 
+		<footer>
+			<p>&copy; {new Date().toLocaleTimeString} Cathy H Lee. All Rights Reserved.</p>
+		</footer>
+	}
+}
