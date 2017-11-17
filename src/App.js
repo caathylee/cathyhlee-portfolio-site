2850d4548eb921bd3d6071916d2df12f8ea5b3d0
Cathy Lee <cathylee@Cathys-MacBook-Pro.local>
2017-11-16 16:00:35 -0800

    Made about section responsive

--- src/App.js
+++ src/App.js
@@ -87,17 +87,18 @@ class App extends Component {
             </div>
           </a>
         </div>
+        <ThreeColoredLine />
         <div className="container-fluid projects" id="projects">
           <h2>Projects</h2>
           <p>Coming soon.</p>
         </div>
-        <ThreeColoredLine />
         <div className="about" id="about">
           <div className="container">
+            <img src={require('./images/cathylee.jpg')} />
             <div className="text">
-            <h2>About Cathy Lee</h2>
-            <p>Cathy Lee is a user-driven web developer that is passionate about creating seamless user interface and experience with a robust and modular application design. She can work autonomously, but thrives in team environments and believes version control as a necessity for efficiency. She currently resides in Los Angeles, California with her two pups. A few technologies she's proficient with include HTML, SASS, JavaScript, jQuery, React, Foundation, Node, Express, PHP, MongoDB, mySQL, and Git.</p>
-          </div>
+              <h2>About Cathy Lee</h2>
+              <p>Cathy Lee is a user-driven web developer that is passionate about creating seamless user interface and experience with a robust and modular application design. She can work autonomously, but thrives in team environments and believes version control as a necessity for efficiency. She currently resides in Los Angeles, California with her two pups. A few technologies she's proficient with include HTML, SASS, JavaScript, jQuery, React, Foundation, Node, Express, PHP, MongoDB, mySQL, and Git.</p>
+            </div>
           </div>
         </div>
         <ThreeColoredLine />
