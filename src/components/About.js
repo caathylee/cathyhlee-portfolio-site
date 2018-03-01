fdb95680cf9942f7075c487d5deb11c7d0833ae8
Cathy Lee <cathylee@cathys-mbp.lan>
2018-02-28 19:21:22 -0800

    Edited bio

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ class About extends Component {
             <img src={require('./../images/cathylee.jpg')} />
             <div className="text">
               <h2>About Cathy Lee</h2>
-              <p>My interest in coding started with my marketing career. I was put in roles where I had to figure out WordPress and DreamWeaver by myself and create webpages. However, I always wanted to figure out how to do websites from scratch. After leaving my marketing job, I helped a startup design and develop their company website, attended Hack Reactor to further my developer skills, and designed responsive websites at an internet marketing company and website design agency. I'm currently searching for a new role as either a Front End or Full Stack Developer that challenges me and furthers my coding skills. I code in React, JavaScript, Bootstrap, have experience in responsive web design and am a wiz at CSS/SASS.</p>
+              <p>My interest in coding started with my marketing career. I was put in roles where I had to figure out WordPress and DreamWeaver by myself and create webpages. However, I always wanted to learn how to do websites from scratch. After leaving my marketing job, I helped a startup design and develop their company website, attended Hack Reactor to further my developer skills, and designed responsive websites at an internet marketing company and website design agency. I'm currently searching for a new role as either a Front End or Full Stack Developer that challenges me and furthers my coding skills. I code in React, JavaScript, Bootstrap, have experience in responsive web design and am a wiz at CSS/SASS.</p>
             </div>
           </div>
         </div>
