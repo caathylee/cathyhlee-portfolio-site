bf04597f06071b5dd80b673ed09d0d08af8f7383
Cathy Lee <cathylee@cathys-mbp.lan>
2018-02-28 19:18:01 -0800

    Added react-typing-game under projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,38 +5,47 @@ class Projects extends Component {
 		return (
     	<div className="container-fluid projects" id="projects">
           <div className="container-fluid personal-projects">
-            <h2>Projects</h2>
-            <p>New personal projects coming soon.</p>
+            <h2>Current Projects</h2>
+            <p className="disclaimer">Below are projects Cathy is currently working on.</p>
+            
+            <div className="one-project">
+              <h3>React Typing Game (WIP)</h3>
+              <p>Typing game tracking user's words per minute.</p>
+              <p>Technologies: React, HTML5, CSS3/SASS, JavaScript, Webpack</p>
+              <a className="btn" href="https://react-typing-game.herokuapp.com/" target="_blank">View Demo</a>
+              <a className="btn" href="https://github.com/caathylee/typing-game-react" target="_blank">View on Github</a>
+              <div className="clearfix"></div>
+              <a className="img-wrapper" href="https://react-typing-game.herokuapp.com/" target="_blank">
+                <img className="hr" src={require('./../images/hr-projects/react-typing-game.png')} />
+              </a>
+            </div>
           </div>
 
-          <p className="disclaimer">Below are school projects Cathy worked on in 2016.</p>
-          <div className="one-project">
-            <h3>ShareCrow</h3>
-            <p>Digital marketplace for users to rent and lend out items with payment security.</p>
-            <p>Technologies: React, Redux, Webpack, Node, Express, Postgres, Redis, AWS S3, Google Maps API, Google Geolocation API, Twilio API, Stripe API, SASS</p>
-            <a className="btn" href="https://github.com/jaquire/share_crow" target="_blank">View on Github</a>
-            <img className="hr" src={require('./../images/hr-projects/sharecrow-landing.png')} />
-            <img className="hr" src={require('./../images/hr-projects/sharecrow-marketplace.png')} />
-          </div>
-          <div className="one-project">
-            <h3>Memowise</h3>
-            <p>A customizable gamified flash card application.</p>
-            <p>Technologies: React, Redux, Node, Express, Postgres</p>
-            <a className="btn" href="https://github.com/AgreeableZombie/Memowise" target="_blank">View on Github</a>
-            <img className="hr" src={require('./../images/hr-projects/memowise.png')} />
-          </div>
-          <div className="one-project">
-            <h3>Read with Me</h3>
-            <p>Application that enables parents to read interactively with children remotely.</p>
-            <p>Technologies: React, Webpack, Node, Express, Twilio, MongoDB</p>
-            <a className="btn" href="https://github.com/AgreeableZombie/AgreeableZombie" target="_blank">View on Github</a>
-            <img className="hr" src={require('./../images/hr-projects/read-with-me.png')} />
-          </div>
-          <div className="one-project">
-            <h3>TsumTsum Typing</h3>
-            <p>Application that delivers a playful approach to typing practice.</p>
-            <p>Technologies: Angular, Node, Express, MongoDB</p>
-            <img className="hr" src={require('./../images/hr-projects/tsumtsumtyping.png')} />
+          <div className="container-fluid school-projects">
+            <h2>Past Projects</h2>
+            <p className="disclaimer">Below are school projects Cathy worked on in 2016.</p>
+            <div className="one-project">
+              <h3>ShareCrow</h3>
+              <p>Digital marketplace for users to rent and lend out items with payment security.</p>
+              <p>Technologies: React, Redux, JavaScript, Webpack, Node, Express, Postgres, Redis, AWS S3, Google Maps API, Google Geolocation API, Twilio API, Stripe API, SASS, Bootstrap</p>
+              <a className="btn" href="https://github.com/jaquire/share_crow" target="_blank">View on Github</a>
+              <img className="hr" src={require('./../images/hr-projects/sharecrow-landing.png')} />
+              <img className="hr" src={require('./../images/hr-projects/sharecrow-marketplace.png')} />
+            </div>
+            <div className="one-project">
+              <h3>Memowise</h3>
+              <p>A customizable gamified flash card application.</p>
+              <p>Technologies: React, Redux, JavaScript, Node, Express, Postgres, Material Design</p>
+              <a className="btn" href="https://github.com/AgreeableZombie/Memowise" target="_blank">View on Github</a>
+              <img className="hr" src={require('./../images/hr-projects/memowise.png')} />
+            </div>
+            <div className="one-project">
+              <h3>Read with Me</h3>
+              <p>Application that enables parents to read interactively with children remotely.</p>
+              <p>Technologies: React, Webpack, Node, Express, Twilio, MongoDB</p>
+              <a className="btn" href="https://github.com/AgreeableZombie/AgreeableZombie" target="_blank">View on Github</a>
+              <img className="hr" src={require('./../images/hr-projects/read-with-me.png')} />
+            </div>
           </div>
       </div>
     	)
